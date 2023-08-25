d84a95668d3bcb5ce80a3ef7379ac29d8746e272
Rajesh Reddy Bejadi <64033035+BejadiRajeshReddy18@users.noreply.github.com>
2023-08-25 05:55:40 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+// import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello,World - Flimpire</h1>
+    </div>
+  );
+};
+
+export default App;
